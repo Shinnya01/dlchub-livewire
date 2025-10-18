e82cecaeda130d25623bf30d8f723414025f9f8b
Shinnya01 <carreon.carll@gmail.com>
2025-10-18 14:31:17 +0800

    old test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,12 +1,6 @@
 <?php
 
 use Livewire\Volt\Volt;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
-
-
-
-
 
 test('registration screen can be rendered', function () {
     $response = $this->get('/register');
@@ -15,19 +9,16 @@ test('registration screen can be rendered', function () {
 });
 
 test('new users can register', function () {
-    Storage::fake('public');
-    $photo = UploadedFile::fake()->image('avatar.jpg');
     $response = Volt::test('auth.register')
         ->set('name', 'Test User')
         ->set('email', 'test@example.com')
         ->set('password', 'password')
         ->set('password_confirmation', 'password')
-        ->set('photo', $photo)
         ->call('register');
 
     $response
         ->assertHasNoErrors()
-        ->assertRedirect(route(url('/'), absolute: false));
+        ->assertRedirect(route('dashboard', absolute: false));
 
     $this->assertAuthenticated();
 });
